3a40e84397189cce1bed754d6d6a98773d4090ea
MarinaRohrig <marinalarissacarpesrohrig@gmail.com>
2022-04-21 13:24:28 -0300

    Primeiro Commit

---
+++ MeuArquivo.py
@@ -0,0 +1,9 @@
+# Analisar dados e enviar relatório por e-mail.
+# Autor: Marina
+
+# Lógica do código:
+# Acessar dados de um arquivo XLS, fazer cálculos com os dados do arquivo, gerar um relatório e enviar o relatório via e-mail
+
+# 1. Importar a base de dados, arquivo Vendas.xls;
+# 2. Visualizar a base de dados;
+# 3. Fazer os cálculos de faturamento da loja, 
