180e0eddd163a39787d4c0c7fe4b8ebe164847d7
MarinaRohrig <marinalarissacarpesrohrig@gmail.com>
2022-04-21 13:54:09 -0300

    Importei arquivo xls, import blib e começo do código

--- MeuArquivo.py
+++ MeuArquivo.py
@@ -6,4 +6,11 @@
 
 # 1. Importar a base de dados, arquivo Vendas.xls;
 # 2. Visualizar a base de dados;
-# 3. Fazer os cálculos de faturamento da loja, 
+# 3. Fazer os cálculos de faturamento da loja. Faturamento = preço de venda x quantidade vendida.
+# 4. Fazer os cálculos de quantidade de produto vendidos por loja;
+# 5. Fazer os cálculos de ticket médio de cada produto, por loja. O ticket médio é o faturamento / número de vendas;
+# 6. Gerar relatórios com base nos resultados dos cálculos;
+# 7. Enviar por e-mail os relatórios.
+
+import pandas as pd
+#vai importar a biblioteca pandas, que lida com planilhas e trata ela como pd
