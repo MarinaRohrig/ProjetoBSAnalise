80d24cb5e5bc8bcb1583effa4693506ccb0b0107
MarinaRohrig <marinalarissacarpesrohrig@gmail.com>
2022-04-25 19:44:07 -0300

    Melhorias de visualização e testes

--- MeuArquivo.py
+++ MeuArquivo.py
@@ -27,32 +27,42 @@ fat = tabela_vendas[['ID Loja','Valor Final']].groupby('ID Loja').sum()
 prodvend = tabela_vendas[['ID Loja','Quantidade']].groupby('ID Loja').sum()
 # ticket medio e transforma em tabela o resultado com to_frame
 ticket_medio = (fat['Valor Final'] / prodvend['Quantidade']).to_frame()
+# rename para escrever o nome da coluna que está mostrando valor
+ticket_medio = ticket_medio.rename(columns={0:'Ticket Médio'})
+# testes, pode comentar todos os prints se não quiser os testes
+print('*-'*30)
+print('Ticket Médio:')
 print('*-'*30)
 print(ticket_medio)
 print('*-'*30)
-# enviar e-mail, via outlook
+print('Produtos Vendidos:')
+print('*-'*30)
+print(prodvend)
+print('*-'*30)
+print('Faturamento:')
+print('*-'*30)
+print(fat)
+print('*-'*30)
+# enviar e-mail, via outlook, precisa ter outlook instalado no pc
 outlook = win32.Dispatch('outlook.application')
 mail = outlook.CreateItem(0)
+# marcar para quem vai receber
 mail.To = 'marinalarissacarpesrohrig@gmail.com'
+# título do e-mail
 mail.Subject = 'Relatório de Vendas ProjetoBSAnalise'
-mail.HTMLBody ='''
-Prezados, 
-Segue o relatório de vendas, por loja, conforme solicitado. 
-Faturamento, por loja: 
-{}
-
-
-Quantidade de produtos vendidos, por loja:
-{}
-
-
-Ticket médio dos produtos, em cada loja: 
-{}
-
-
-Para dúvidas estou a disposição. 
-Att. Marina ~~
+# coloquei f na frente para poder usar {} para mostrar variáveis no meio do texto, <p> para criar paragrafos
+mail.HTMLBody = f'''
+<p> Prezados,</p>
+<p> Segue o relatório de vendas, por loja, conforme solicitado. </p>
+<p> Faturamento, por loja: </p>
+{fat.to_html(formatters={'Valor Final': 'R${:.2f}'.format})}
+<p> Quantidade de produtos vendidos, por loja:</p>
+{prodvend.to_html()}
+<p> Ticket médio dos produtos, em cada loja: </p>
+{ticket_medio.to_html(formatters={'Ticket Médio': 'R${:.2f}'.format})}
+<p> Para dúvidas estou a disposição. </p>
+<p> Att. Marina</p> ~~
 '''
 mail.Display()
-print ('Pronto para ser enviado!')
+print('E-mail pronto para ser enviado')
 
