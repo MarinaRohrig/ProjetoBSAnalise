906940775d540e834ce52e09e8cb4f1efaf7b157
MarinaRohrig <marinalarissacarpesrohrig@gmail.com>
2022-04-25 15:50:13 -0300

    Configurado envio de e-mail via outlook, e-mail cru

--- MeuArquivo.py
+++ MeuArquivo.py
@@ -1,6 +1,6 @@
 # Analisar dados e enviar relatório por e-mail.
 # Autor: Marina
-
+# Packages: pandas, pywin32 e openpyxl
 # Lógica do código:
 # Acessar dados de um arquivo XLS, fazer cálculos com os dados do arquivo, gerar um relatório e enviar o relatório via e-mail
 
@@ -12,8 +12,10 @@
 # 6. Gerar relatórios com base nos resultados dos cálculos;
 # 7. Enviar por e-mail os relatórios.
 
-# vai importar a biblioteca pandas, que lida com planilhas e trata ela como pd
+# importa a biblioteca pandas, que lida com planilhas e trata ela como pd
 import pandas as pd
+# importa a biblioteca que lida com aplicações do windows
+import win32com.client as win32
 
 # importa o excel
 tabela_vendas = pd.read_excel('Vendas.xlsx')
@@ -28,6 +30,29 @@ ticket_medio = (fat['Valor Final'] / prodvend['Quantidade']).to_frame()
 print('*-'*30)
 print(ticket_medio)
 print('*-'*30)
+# enviar e-mail, via outlook
+outlook = win32.Dispatch('outlook.application')
+mail = outlook.CreateItem(0)
+mail.To = 'marinalarissacarpesrohrig@gmail.com'
+mail.Subject = 'Relatório de Vendas ProjetoBSAnalise'
+mail.HTMLBody ='''
+Prezados, 
+Segue o relatório de vendas, por loja, conforme solicitado. 
+Faturamento, por loja: 
+{}
 
 
+Quantidade de produtos vendidos, por loja:
+{}
+
+
+Ticket médio dos produtos, em cada loja: 
+{}
+
+
+Para dúvidas estou a disposição. 
+Att. Marina ~~
+'''
+mail.Display()
+print ('Email enviado!')
 
